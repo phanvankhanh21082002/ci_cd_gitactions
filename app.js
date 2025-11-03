556d5709370e7641131f21d7b63e45f924e9b6d8
phanvankhanh21082002 <khanh0918992880@gmail.com>
2025-11-03 13:21:51 +0700

    second push

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello Khanh from STAGING environment (CI/CD + Docker + GitHub Actions)!\n");
+  res.end("Hello Khanh1 from STAGING environment (CI/CD + Docker + GitHub Actions)!\n");
 });
 
 server.listen(PORT, () => {
